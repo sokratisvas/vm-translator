f09948e1d060679a12ce7f70c34e8d5891f4eeee
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-07-17 22:26:25 +0300

    start with arithmetic commands

--- src/vm_translator.py
+++ src/vm_translator.py
@@ -3,18 +3,36 @@ segments = {
     'static': '16', 'temp': '5', 'pointer': ['THIS', 'THAT']
 }
 
+def arithmetic_command(token):
+    match token:
+        case 'add':
+            assembly = ['@SP', 'M=M-1', 'A=M', 
+                        'D=M', 'A=A-1', 'M=M+D']
+        case 'sub':
+            assembly = ['@SP', 'M=M-1', 'A=M', 
+                        'D=M', 'A=A-1', 'M=M-D']
+        case 'neg':
+            assembly = ['@SP', ]
+
+
+
+#def memory_command(token)
+
 def tokenize(filename):
     filepath = '../examples/' + filename
     with open(filepath, mode = 'r', encoding = 'utf-8') as f:
-        instructions = [line.strip() for line in f if line.strip()[:2] != '//' and line.strip()]
+        instructions = [line.strip() for line in f 
+                        if line.strip()[:2] != '//' and line.strip()]
 
     tokens = [instruction.split() for instruction in instructions]
 
     for token in tokens:
         print(token)
 
-def write_assembly(tokens):
-    stack = []
+#def write_assembly(tokens):
+#    for token in tokens:
+#        assert (len(token) == 1 or len(token) == 3)
+            
 
 
 
